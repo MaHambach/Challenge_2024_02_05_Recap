ea8d623c337df70da21701f4c43ef23140f10124
mahambach <ma.hambach@gmx.net>
2024-02-06 12:15:32 +0100

    install AssertJ add TestClass ShopServiceAssert

---
+++ src/test/java/org/github/mahambach/ShopServiceTest.java
@@ -0,0 +1,48 @@
+package org.github.mahambach;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopServiceTest {
+
+    @Test
+    void placeOrder() {
+    }
+
+    @Test
+    void getProductRepo() {
+    }
+
+    @Test
+    void getOrderRepo() {
+    }
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void testAddProduct() {
+    }
+
+    @Test
+    void getAllOrders() {
+    }
+
+    @Test
+    void stockProductByID() {
+    }
+
+    @Test
+    void getOrderByID() {
+    }
+
+    @Test
+    void getAllOrdersOfCustomerID() {
+    }
+
+    @Test
+    void getProductByID() {
+    }
+}
---
+++ src/test/java/org/github/mahambach/ShopServigeAssert.java
@@ -0,0 +1,12 @@
+package org.github.mahambach;
+
+import org.assertj.core.api.AbstractAssert;
+
+public class ShopServigeAssert extends AbstractAssert<ShopServigeAssert, ShopService> {
+
+    protected ShopServigeAssert(ShopService shopService, Class<?> selfType) {
+        super(shopService, selfType);
+    }
+
+
+}
