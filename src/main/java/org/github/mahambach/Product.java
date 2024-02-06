803138cb8b987b1efa5f54317e99795d2e448552
mahambach <ma.hambach@gmx.net>
2024-02-06 12:07:08 +0100

    add tests for ProductRepo

--- src/main/java/org/github/mahambach/Product.java
+++ src/main/java/org/github/mahambach/Product.java
@@ -1,6 +1,7 @@
 package org.github.mahambach;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public record Product(
         int productID,
@@ -10,5 +11,18 @@ public record Product(
     @Override
     public String toString() {
         return String.format("Product{ ID = %6d, productName = %19s, Price= %8s €}",productID, productName, productPrice);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productID == product.productID && Objects.equals(productName, product.productName) && Objects.equals(productPrice, product.productPrice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productID, productName, productPrice);
     }
 }
--- src/test/java/org/github/mahambach/ProductRepoTest.java
+++ src/test/java/org/github/mahambach/ProductRepoTest.java
@@ -68,30 +68,127 @@ class ProductRepoTest {
     }
 
     @Test
-    void getProductQuantityByName() {
+    void getProductQuantityByName_whenLaptop_then10() {
+        ProductRepo productRepo = new ProductRepo();
+        List<Product> productList = new ArrayList<>();
+        productList.add( new Product(1, "Laptop", new BigDecimal("999.99")));
+        productList.add( new Product(2, "Smartphone", new BigDecimal("599.95")));
+        List<Integer> quantities = new ArrayList<>();
+        quantities.add(10);
+        quantities.add(100);
+        int numberOfProducts = productRepo.getProductList().size();
+
+        productRepo.addProduct(productList, quantities);
+
+        assertEquals(10, productRepo.getProductQuantityByName("Laptop"));
+    }
+    @Test
+    void getProductQuantityByName_whenPotato_then0() {
+        ProductRepo productRepo = new ProductRepo();
+        List<Product> productList = new ArrayList<>();
+        productList.add( new Product(1, "Laptop", new BigDecimal("999.99")));
+        productList.add( new Product(2, "Smartphone", new BigDecimal("599.95")));
+        List<Integer> quantities = new ArrayList<>();
+        quantities.add(10);
+        quantities.add(100);
+
+        productRepo.addProduct(productList, quantities);
+
+        assertEquals(0, productRepo.getProductQuantityByName("Potato"));
     }
 
     @Test
-    void getProductByName() {
+    void getProductByName_whenLaptop_thenProduct1() {
+        ProductRepo productRepo = new ProductRepo();
+        List<Product> productList = new ArrayList<>();
+        Product product1 = new Product(1, "Laptop", new BigDecimal("999.99"));
+        Product product2 = new Product(2, "Smartphone", new BigDecimal("599.95"));
+        productList.add(product1);
+        productList.add(product2);
+        List<Integer> quantities = new ArrayList<>();
+        quantities.add(10);
+        quantities.add(100);
+
+        productRepo.addProduct(productList, quantities);
+
+        assertEquals(product1, productRepo.getProductByName("Laptop"));
     }
 
     @Test
-    void getProductByID() {
+    void getProductByID_when2_thenProduct2() {
+        ProductRepo productRepo = new ProductRepo();
+        List<Product> productList = new ArrayList<>();
+        Product product1 = new Product(1, "Laptop", new BigDecimal("999.99"));
+        Product product2 = new Product(2, "Smartphone", new BigDecimal("599.95"));
+        productList.add(product1);
+        productList.add(product2);
+        List<Integer> quantities = new ArrayList<>();
+        quantities.add(10);
+        quantities.add(100);
+
+        productRepo.addProduct(productList, quantities);
+
+        assertEquals(product2, productRepo.getProductByID(2));
     }
 
     @Test
-    void getAllProductsByPartialName() {
+    void getAllProductsByPartialName_whena_thenLaptopAndSmartphone() {
+        ProductRepo productRepo = new ProductRepo();
+        List<Product> productList = new ArrayList<>();
+        Product product1 = new Product(1, "Laptop", new BigDecimal("999.99"));
+        Product product2 = new Product(2, "Smartphone", new BigDecimal("599.95"));
+        productList.add(product1);
+        productList.add(product2);
+        List<Integer> quantities = new ArrayList<>();
+        quantities.add(10);
+        quantities.add(100);
+
+        productRepo.addProduct(productList, quantities);
+
+        assertEquals(productList, productRepo.getAllProductsByPartialName("a"));
+    }
+
+
+    @Test
+    void getAllProductsByPartialName_whenx_thenEmpty() {
+        ProductRepo productRepo = new ProductRepo();
+        List<Product> productList = new ArrayList<>();
+        Product product1 = new Product(1, "Laptop", new BigDecimal("999.99"));
+        Product product2 = new Product(2, "Smartphone", new BigDecimal("599.95"));
+        productList.add(product1);
+        productList.add(product2);
+        List<Integer> quantities = new ArrayList<>();
+        quantities.add(10);
+        quantities.add(100);
+
+        productRepo.addProduct(productList, quantities);
+
+        assertEquals(new ArrayList<>(), productRepo.getAllProductsByPartialName("x"));
     }
 
     @Test
-    void getAllProducts() {
+    void getAllProducts_whenLaptopAndSmartphone_thenLaptopAndSmartphone() {
+        ProductRepo productRepo = new ProductRepo();
+        List<Product> productList = new ArrayList<>();
+        Product product1 = new Product(1, "Laptop", new BigDecimal("999.99"));
+        Product product2 = new Product(2, "Smartphone", new BigDecimal("599.95"));
+        productList.add(product1);
+        productList.add(product2);
+        List<Integer> quantities = new ArrayList<>();
+        quantities.add(10);
+        quantities.add(100);
+
+        productRepo.addProduct(productList, quantities);
+
+        assertEquals(productList, productRepo.getAllProducts());
     }
 
-    @Test
-    void getProductList() {
-    }
 
     @Test
-    void getProductQuantities() {
+    void getAllProducts_whenEmpty_thenEmpty() {
+        ProductRepo productRepo = new ProductRepo();
+
+
+        assertTrue(productRepo.getAllProducts().isEmpty());
     }
 }
